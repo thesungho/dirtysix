6e82f2dd2f5b9ca8f8483890319be91babcb8ab3
Sungho Yoo <sunghoster@gmail.com>
2013-07-30 18:23:51 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sixthstreet::Application.config.secret_token = '487ec732454c40339eed8780b09ce2f62e27feed445418b50845e0a391f955f6a0b93d2eb43c0ba315011d808b561e5f6eb4de4be6016fdba4e30df77b7738ab'
