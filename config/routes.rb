b332f09de008ea86c5a40b36528951c164424641
Sungho Yoo <sunghoster@gmail.com>
2013-07-31 17:23:56 -0500

    Bare bone version ofapp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Sixthstreet::Application.routes.draw do
+
+get 'about' => 'pages#about'
+  
+root :to => 'pages#home'
+
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
